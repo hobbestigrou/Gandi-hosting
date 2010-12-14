9c17f4f36f6ae06149d252f02626e5a1afa50f4d
Natal Ngétal <hobbestigrou@erakis.im>
2010-12-14 11:44:32 +0100

    Add getopt and switch, and add function vmlist.

--- gandi-hosting.pl
+++ gandi-hosting.pl
@@ -21,35 +21,46 @@
 use strict;
 use warnings;
 use Net::Gandi;
+use Getopt::Long;
+use Switch;
 
-my $vm       = Net::Gandi::Hosting::VM->new(apikey => 'd7iDsBNAw1U3AV6UuQy7v6Hs');
-my $vm_lists = $vm->list();
+my %opts;
+GetOptions ( \%opts, "vmlist");
 
-if ( ! $vm_lists ) {
-    print "You don't have vm";
-    return;
+switch (%opts) {
+    case ('vmlist')  { vm_list() }
 }
-else {
-    foreach my $vm_list (@{$vm_lists}) {
-	while (my ( $key, $value ) = each(%$vm_list)) {
-	    if ( $key =~ m/name/ ) {
-		print "Information about $value ";
+
+sub vm_list {
+    my $vm       = Net::Gandi::Hosting::VM->new(apikey => 'd7iDsBNAw1U3AV6UuQy7v6Hs');
+    my $vm_lists = $vm->list();
+
+    if ( ! $vm_lists ) {
+        print "You don't have vm";
+        return;
+    }
+    else {
+        foreach my $vm_list (@{$vm_lists}) {
+	    while (my ( $key, $value ) = each(%$vm_list)) {
+	        if ( $key =~ m/name/ ) {
+		    print "Information about $value ";
+	        }
+                if ( $key =~ m/disks_id/ || $key =~ m/ifaces_id/ ) {
+		    my $name = "Disks lists: ";
+		    if ( $key  =~ m/ifaces_id/ ) {
+		        $name = "Ifaces lists: ";
+		    }
+		    foreach my $array (@{$vm_list->{$key}}) {
+		        print "$name $array, ", "\n";
+		    }
+	        }
+	       else {
+	           if ( ! $value ) {
+		       $value = 'None';
+		   }
+		   print "$key: $value", "\n";
+	       } 
 	    }
-	    if ( $key =~ m/disks_id/ || $key =~ m/ifaces_id/ ) {
-		my $name = "Disks lists: ";
-		if ( $key  =~ m/ifaces_id/ ) {
-		    $name = "Ifaces lists: ";
-		}
-		foreach my $array (@{$vm_list->{$key}}) {
-		    print "$name $array, ", "\n";
-		}
-	    }
-	    else {
-		if ( ! $value ) {
-		    $value = 'None';
-		}
-		print "$key: $value", "\n";
-	   }
-	}
+        }
     }
 }
